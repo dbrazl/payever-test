f13eecd062143c3fac9828a190ff8c4944825255
Daniel Braz <danielbrazlopes16@gmail.com>
2023-03-10 08:53:43 -0300

    feat: create calendar and drag drop task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'payever-test';
+  dates = new Array(30).fill(0).map((_, index) => index + 1);
+  dateTasks = new Array(30).fill(null).map(_ => ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep']);
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
